c88f6799f8e924b7bbe12a8c1a4c45fe8616d83d
Jessica Asamoah <jessicaasamoah903@gmail.com>
2023-10-18 23:58:08 +0000

    More queries sql

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,7 @@
+-- Creates database hbtn_0d_2 and the user user_0d_2
+-- Creates database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- Creates user
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+-- Grants SELECT privileges
+GRANT SELECT ON hbtn_0d_2.* TO USER user_0d_2@localhost;

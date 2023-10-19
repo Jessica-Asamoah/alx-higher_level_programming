39ea14cd293f87d00a7e77d677ff92b005283f89
Jessica Asamoah <jessicaasamoah903@gmail.com>
2023-10-19 00:09:54 +0000

    More queries sql

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- Creates the database hbtn_0d_usa and the table states on MySQL server
+-- Creates database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- Use database
+USE hbtn_0d_usa;
+-- Creates table
+CREATE TABLE IF NOT EXISTS states (id INT UNIQUE NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
